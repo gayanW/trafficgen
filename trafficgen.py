3635e7483b3a43ce28edae2440f815f09d088bca
Gayan Weerakutti <gayan@linuxdeveloper.space>
2016-08-05 21:05:59 +0530

    Update

--- trafficgen.py
+++ trafficgen.py
@@ -1,20 +1,30 @@
 from urllib.request import urlopen
 from urllib.request import Request
-from bs4 import BeautifulSoup
+import time
+#from bs4 import BeautifulSoup
 
-url = "https://www.whatismybrowser.com/developers/what-http-headers-is-my-browser-sending"
+#url = "https://www.whatismybrowser.com/developers/what-http-headers-is-my-browser-sending"
 #url = "http://www.linuxdeveloper.space"
+url = 'http://www.gossipocean.com'
 
 # http headers
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-referer = 'https://www.facebook.com/'
-headers = {'User-Agent':user_agent, 'Referer':referer}
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome'
+referers = ['https://www.facebook.com',
+            'http://www.linuxdeveloper.space',
+            'https://twitter.com']
 
-req = Request(url, headers=headers)
-response = urlopen(req)
+# send request to url by each referer
 
-bsObj = BeautifulSoup(response.read(), 'lxml')
+for referer in referers:
+    print('referer', referer)
+    headers = {'User-Agent':user_agent, 'Referer':referer}
+    req = Request(url, headers=headers)
+    response = urlopen(req)
+
+    print('{} >>\n'.format(referer), response.getheaders())
+    time.sleep(10)
 
 
-tableText = bsObj.find('table', {'class':'table-striped'}).get_text()
-print(tableText)
+#bsObj = BeautifulSoup(response.read(), 'lxml')
+#tableText = bsObj.find('table', {'class':'table-striped'}).get_text()
+#print(tableText)
